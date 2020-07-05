73479a52b5a3785ad5e2b91c21717e7e5bc3e08c
Jeremy Hettenhouser <jeremy.hettenhouser@itgfirm.com>
2020-07-05 19:14:00 -0400

    Added parameters to the yaml and updated syntax

--- notify/notify.py
+++ notify/notify.py
@@ -4,6 +4,7 @@ import pychromecast
 from gtts import gTTS
 from simhash import Simhash
 
+
 #
 # Notify
 #
@@ -11,68 +12,60 @@ from simhash import Simhash
 #
 # Args:
 #
-
-HOST_NAME = "192.168.2.6"
-HOST_PORT = 8123
-
-ENDPOINT_MSG = "notify"
-BROADCAST_GRP = "Home group"
-
-DOCKER_CONFIG_DIR = "/config"
-
-
 class Notify(hass.Hass):
 
     def initialize(self):
-        self.log("Initializing endpoint")
-        self.register_endpoint(self.handle_request, "notify")
+        self.log(f"Initializing Notify endpoint with args {self.args}")
+        self.register_endpoint(self.handle_request, self.args['endpoint'])
 
-        if not os.path.exists(DOCKER_CONFIG_DIR + "/www/audio/appdaemon"):
-            os.makedirs(DOCKER_CONFIG_DIR + "/www/audio/appdaemon")
+        if not os.path.exists(self.args['tts_cache_dir']):
+            os.makedirs(self.args['tts_cache_dir'])
 
     def handle_request(self, data):
         self.log(data)
 
-        self.notify(str(data["message"]))
+        self.notify(str(data['message']))
 
-        return 200
+        return None, 200
 
     def notify(self, notification):
-        if notification == "":
-            notification = "No+Notification+Data+Received"
+        if notification == '':
+            notification = 'No+Notification+Data+Received'
 
-        mp3 = DOCKER_CONFIG_DIR + "/www/audio/appdaemon/" + str(Simhash(notification).value) + ".mp3"
-        mp3_url = "http://" + HOST_NAME + ":" + str(HOST_PORT) + "/local/audio/appdaemon/" + str(Simhash(notification).value) + ".mp3"
+        mp3_file_hash = str(Simhash(notification).value)
+        mp3 = f"{self.args['tts_cache_dir']}/{mp3_file_hash}.mp3"
+        mp3_url = f"http://{self.args['host_name']}{self.args['tts_www_dir']}/{mp3_file_hash}.mp3"
         self.log(mp3)
         self.log(mp3_url)
         text = notification.replace("+", " ")
 
         if not os.path.isfile(mp3):
-            self.log("Generating MP3...")
+            self.log('Generating MP3...')
             tts = gTTS(text=text,
-                       lang='en-us')  # See Google TTS API for more Languages (Note: This may do translation Also - Needs Testing)
+                       lang='en-us')
+            # save mp3 file to the HA config directory
             tts.save(mp3)
         else:
-            self.log("Reusing MP3...")
+            self.log('Reusing MP3...')
 
-        self.Cast(mp3_url)
+        self.cast(mp3_url)
 
-        self.log("Notification Sent.")
+        self.log('Notification Sent.')
 
-    def Cast(self, mp3_url):
+    def cast(self, mp3):
 
-        casts, browser = pychromecast.get_listed_chromecasts(friendly_names=[BROADCAST_GRP])
+        casts, browser = pychromecast.get_listed_chromecasts(friendly_names=[self.args['broadcast_grp']])
         # Shut down discovery as we don't care about updates
         pychromecast.discovery.stop_discovery(browser)
 
         if len(casts) == 0:
-            self.log("No Devices Found")
+            self.log('No Devices Found')
 
         cast = casts[0]
         cast.wait()
 
         mc = cast.media_controller
-        mc.play_media(mp3_url, "audio/mp3")
+        mc.play_media(mp3, 'audio/mp3')
         mc.block_until_active()
 
     def terminate(self):
